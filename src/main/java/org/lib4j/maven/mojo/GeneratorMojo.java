a18e6e4a167f1ecea06d55fc74564db57fa903e0
Seva Safris <seva@safris.org>
2018-02-27 23:06:05 +0700

    Remove unused import

--- src/main/java/org/lib4j/maven/mojo/GeneratorMojo.java
+++ src/main/java/org/lib4j/maven/mojo/GeneratorMojo.java
@@ -21,7 +21,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
